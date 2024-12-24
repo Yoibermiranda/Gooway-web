57b9a841705185191358c6f456462f48f98b157d
freider3 <freidersimarra83@gmail.com>
2024-12-24 09:18:38 -0500

    carrusel

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
-
+import Carousel from '../src/components/carrusel';
 function App() {
+
+  const images = [
+    'https://via.placeholder.com/600x300?text=Slide+1',
+    'https://via.placeholder.com/600x300?text=Slide+2',
+    'https://via.placeholder.com/600x300?text=Slide+3',
+];
+
   return (
     <div className="App">
       <h1>Hello Word</h1>
+
+      <Carousel images={images} />
     </div>
+
   );
 }
 
